8b7f2f519404a183794398e5c251a24f0da90c37
konstantinnovation <konstans@stuy.edu>
2022-10-18 12:41:10 -0400

    Create Driver.java

---
+++ Driver.java
@@ -0,0 +1,47 @@
+public class Driver{
+  public static void main(String[]args){
+    //Uncomment each line of code as you write the methods
+    
+    //0.CONSTRUCTORS
+    //constructor that takes the name and age of the Kitty.
+    /*
+    Kitty a = new Kitty("Matcha", 3);
+    System.out.println(a);
+    */
+    //should print The address. Later it will print "The amazing Matcha."
+    
+    //2nd constructor makes a Kitty named Mittens that is 2 years old
+    //this constructor takes no parameters.
+    /*
+    Kitty b = new Kitty();
+    System.out.println(b);
+    */
+    //should print The address. Later it will print "The amazing Mittens"
+    
+    //1. Accessor Methods
+    /*
+    System.out.println(a.getName() + " is "+a.getAge() + " years old.");
+    //should print "Matcha is 3 years old."
+    */
+    
+    //2. Mutator Method
+    /*
+    a.makeOlder();
+    System.out.println(a.getName() + " is "+a.getAge() + " years old.");
+    should print "Matcha is 4 years old."
+    */
+    
+    //4. Mutator Method
+    /*
+    a.makeOlder();
+    a.changeName("Katya");
+    b.changeName("Aytak");
+    b.makeOlder();
+    System.out.println(a.getName() + " is "+a.getAge() + " years old.");
+    System.out.println(b.getName() + " is " + b.getAge() + " years old.");
+    */
+    //should print "Katya is 5 years old."
+    //should print "Aytak is 3 years old."
+        
+  }
+}
